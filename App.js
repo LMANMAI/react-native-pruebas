c1dad8d65907006c7a515799b0a300cae0b10061
Lucas Maidana <64815326+LMANMAI@users.noreply.github.com>
2023-03-21 20:21:32 -0300

    inicio del proyecto, pruebas de curso react native

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>Primera app de prueba React Native</Text>
       <StatusBar style="auto" />
     </View>
   );
